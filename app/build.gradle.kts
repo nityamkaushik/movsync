2209d2f721c13c66e6c968297fd6c5682310778e
nityamkaushik <nityamkaushik@gmail.com>
2026-06-03 22:19:12 +0530

    release 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.nityam.movsync"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
