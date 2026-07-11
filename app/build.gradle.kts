0402375344716192eaf8144d104e690e7d0a9643
nityamkaushik <nityamkaushik@gmail.com>
2026-07-11 19:09:19 +0530

    grad,e to 2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 26
-        versionName = "2.1.2"
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
